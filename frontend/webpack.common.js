4a2e1c85b5d3c28e959b64f0e4ccdf661f9ffa13
GeonwooShin <rjsdnslawkd@naver.com>
2023-11-29 01:00:00 +0900

    refactor: alias 추가 및 로더 추가

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -23,11 +23,14 @@ module.exports = {
     extensions: ['.tsx', '.ts', '.jsx', '.js'],
     alias: {
       '@components': path.resolve(__dirname, 'src/components'),
-      '@assets': path.resolve(__dirname, 'src/assets'),
       '@styles': path.resolve(__dirname, 'src/styles'),
-      '@contexts': path.resolve(__dirname, 'src/contexts'),
+      '@apis': path.resolve(__dirname, 'src/apis'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@constants': path.resolve(__dirname, 'src/constants'),
       '@hooks': path.resolve(__dirname, 'src/hooks'),
       '@utils': path.resolve(__dirname, 'src/utils'),
+      '@public': path.resolve(__dirname, 'public'),
+      '@config': path.resolve(__dirname, 'config.ts'),
     },
   },
   module: {
@@ -44,6 +47,18 @@ module.exports = {
       {
         test: /\.(png|jpe?g|webp)$/,
         type: 'asset',
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack', 'url-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)?$/,
+        type: 'asset',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
