015c9111280886579a302b285c040501fa0db95b
GeonwooShin <rjsdnslawkd@naver.com>
2023-11-29 01:07:52 +0900

    refactor: 헤더 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
-import Button from '@components/Button';
+import Header from '@components/Header';
 
 const App = () => {
   return (
     <div>
-      <Button />
+      <Header />
     </div>
   );
 };
