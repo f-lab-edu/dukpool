0e63ed0bfd0adb24229ccc5e85de859c0d10dff4
GeonwooShin <rjsdnslawkd@naver.com>
2023-11-29 13:12:39 +0900

    feat: 헤더 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,14 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
+import Header from '@components/Header';
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <Header />
+      <RouterProvider router={router} />
+    </>
+  );
 };
 
 export default App;
