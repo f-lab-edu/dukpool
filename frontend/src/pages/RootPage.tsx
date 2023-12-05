e2af9ca97b7b6bc23a762caa15d728428c088da2
GeonwooShin <rjsdnslawkd@naver.com>
2023-12-05 22:51:46 +0900

    refactor: RootPage에 헤더와 푸터 추가 & Outlet 레이아웃 추가

--- frontend/src/pages/RootPage.tsx
+++ frontend/src/pages/RootPage.tsx
@@ -1,11 +1,16 @@
 import { Outlet } from 'react-router-dom';
-
+import Header from '@components/common/Header';
+import Footer from '@components/common/Footer';
+import Layout from '@components/common/Layout';
 const RootPage = () => {
   return (
-    <div>
-      root
-      <Outlet />
-    </div>
+    <>
+      <Header />
+      <Layout>
+        <Outlet />
+      </Layout>
+      <Footer />
+    </>
   );
 };
 
