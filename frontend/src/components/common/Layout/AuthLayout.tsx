f18a4ddf0478dff2bb3a71ef96b5ec332c172411
GeonwooShin <rjsdnslawkd@naver.com>
2023-12-07 22:44:08 +0900

    design: 로그인 유무 레이아웃 디자인 요소 제거

--- frontend/src/components/common/Layout/AuthLayout.tsx
+++ frontend/src/components/common/Layout/AuthLayout.tsx
@@ -1,5 +1,4 @@
 import { memo } from 'react';
-import styled from 'styled-components';
 
 type AuthLayoutProps = {
   children: React.ReactNode;
@@ -8,18 +7,9 @@ type AuthLayoutProps = {
 const AuthLayout = memo(({ children }: AuthLayoutProps) => {
   // 로그인 유무 처리
   // 비로그인 시 로그인 페이지로 이동
-  return <StyledAuthLayout>{children}</StyledAuthLayout>;
+  return <>{children}</>;
 });
 
 AuthLayout.displayName = 'AuthLayout';
-
-const StyledAuthLayout = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: 0px auto;
-  max-width: 1240px;
-  min-height: 100vh;
-  padding-top: 80px;
-`;
 
 export default AuthLayout;
