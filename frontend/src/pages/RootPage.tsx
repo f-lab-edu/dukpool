fa1ab380d4c21ebf9047cab3e00dcbc7e860644f
GeonwooShin <rjsdnslawkd@naver.com>
2023-12-07 22:50:12 +0900

    refactor: media-query에 따라 Footer와 MobileNavbar로 분기

--- frontend/src/pages/RootPage.tsx
+++ frontend/src/pages/RootPage.tsx
@@ -2,14 +2,17 @@ import { Outlet } from 'react-router-dom';
 import Header from '@components/common/Header';
 import Footer from '@components/common/Footer';
 import Layout from '@components/common/Layout';
+import useMediaQuery from '@hooks/useMediaQuery';
+import MobileNavbar from '@components/common/Navbar/MobileNavbar';
 const RootPage = () => {
+  const { isMobile } = useMediaQuery();
   return (
     <>
       <Header />
       <Layout>
         <Outlet />
       </Layout>
-      <Footer />
+      {isMobile ? <MobileNavbar /> : <Footer />}
     </>
   );
 };
