fceb405f6b765906072bc9b286828e4ed1985ec4
GeonwooShin <rjsdnslawkd@naver.com>
2023-12-17 23:18:43 +0900

    feat: context api와 error boundary 컴포넌트 추가

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,19 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/font.css';
+import { AuthProvider } from '@context/AuthContext';
 import GlobalStyles from '@styles/GlobalStyles';
+import ErrorBoundary from '@components/common/ErrorBoundary';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
-  <>
-    <GlobalStyles />
-    <App />
-  </>,
+  <AuthProvider>
+    <ErrorBoundary>
+      <GlobalStyles />
+      <App />
+    </ErrorBoundary>
+  </AuthProvider>,
 );
