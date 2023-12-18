1729df82d5edc0bb551a73390fe65ba93f9d54bc
GeonwooShin <rjsdnslawkd@naver.com>
2023-12-18 23:57:10 +0900

    refactor: AuthProvider & ErrorBoundary 제거

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,19 +1,15 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/font.css';
-import { AuthProvider } from '@context/AuthContext';
 import GlobalStyles from '@styles/GlobalStyles';
-import ErrorBoundary from '@components/common/ErrorBoundary';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
-  <AuthProvider>
-    <ErrorBoundary>
-      <GlobalStyles />
-      <App />
-    </ErrorBoundary>
-  </AuthProvider>,
+  <>
+    <GlobalStyles />
+    <App />
+  </>,
 );
