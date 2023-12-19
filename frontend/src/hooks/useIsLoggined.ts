8fb33f51fbf7ffd2e4d7c741a8d364dc597fcc87
GeonwooShin <rjsdnslawkd@naver.com>
2023-12-19 17:09:01 +0900

    refactor: 에러명 수정

--- frontend/src/hooks/useIsLoggined.ts
+++ frontend/src/hooks/useIsLoggined.ts
@@ -4,7 +4,7 @@ import { AuthContext } from '@context/AuthContext';
 const useIsLoggined = () => {
   const { isLoggined } = useContext(AuthContext);
   if (!isLoggined) {
-    throw new Error('The token does not exist or is not valid.');
+    throw new Error('UnAuthorized Error');
   }
 };
 
