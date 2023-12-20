f5b20c7410a30b73a1101062a0eea1dc41b921e6
xenex90 <xenexes@naver.com>
2023-12-21 04:48:19 +0900

    modify kakao sns login
    - kakao sns login test

--- backend/src/main/java/com/dukpool/backend/SecurityConfig.java
+++ backend/src/main/java/com/dukpool/backend/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-            .antMatchers("/", "/login/**", "/css/**", "/images/**", "/js/**", "/swagger-ui/**", "/v3/api-docs/**","/swagger-resources/**").permitAll()
+            .antMatchers("/", "/login/**", "/css/**", "/images/**", "/js/**", "/swagger-ui/**", "/v3/api-docs/**","/swagger-resources/**","/auth/**").permitAll()
             .anyRequest().authenticated()
             .and()
             .oauth2Login()
