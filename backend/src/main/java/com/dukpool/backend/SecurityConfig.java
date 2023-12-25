a5bc0c217c8c3bbb503f1a4ebfd3ad3d2fb66ba4
xenex90 <xenexes@naver.com>
2023-12-26 01:31:50 +0900

    resolve cors error test3
    -Configuration 추가 (WebConfig)

--- backend/src/main/java/com/dukpool/backend/SecurityConfig.java
+++ backend/src/main/java/com/dukpool/backend/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.dukpool.backend;
 
+import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -16,10 +20,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .authorizeRequests()
             .antMatchers("/", "/login/**", "/css/**", "/images/**", "/js/**", "/swagger-ui/**", "/v3/api-docs/**","/swagger-resources/**","/auth/**").permitAll()
             .anyRequest().authenticated()
@@ -27,5 +30,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .oauth2Login()
             .defaultSuccessUrl("/loginSuccess")
             .failureUrl("/loginFailure");
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // 허용할 오리진
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용할 HTTP 메소드
+        configuration.setAllowedHeaders(Arrays.asList("*")); // 허용할 헤더
+        configuration.setAllowCredentials(true); // Credential 허용
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 위 설정 적용
+        return source;
     }
 }
