aff8cea7e0c3414a8b76ae55d47cf2abecf87d58
xenex90 <xenexes@naver.com>
2023-12-26 01:42:18 +0900

    resolve cors error test4
    -Configuration 추가 (WebConfig)

--- backend/src/main/java/com/dukpool/backend/WebConfig.java
+++ backend/src/main/java/com/dukpool/backend/WebConfig.java
@@ -13,6 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**") // 모든 경로에 대해
             .allowedOrigins("http://localhost:3000") // 특정 오리진 허용
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메소드
-            .allowCredentials(false); // 쿠키나 인증과 관련된 정보를 처리할 때 필요
+            .allowCredentials(false) // 쿠키나 인증과 관련된 정보를 처리할 때 필요
+            .allowedMethods ("*");
     }
 }
