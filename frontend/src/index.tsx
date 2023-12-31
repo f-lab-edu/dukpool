e71159af3c8d0f9a9380b68b59bacc4ea690dcd0
GeonwooShin <rjsdnslawkd@naver.com>
2023-12-31 14:08:15 +0900

    refactor: queryClient context로 분리

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,17 +2,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/font.css';
 import GlobalStyles from '@styles/GlobalStyles';
-import { QueryClient, QueryClientProvider } from 'react-query';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
 
-export const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-  <QueryClientProvider client={queryClient}>
+  <>
     <GlobalStyles />
     <App />
-  </QueryClientProvider>,
+  </>,
 );
