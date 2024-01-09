312bddd545532bc64e4b43d048ab9d97a1af3221
GeonwooShin <rjsdnslawkd@naver.com>
2024-01-09 18:53:12 +0900

    refactor: useQuery 타입 제거

--- frontend/src/hooks/useGetQueries.ts
+++ frontend/src/hooks/useGetQueries.ts
@@ -2,49 +2,40 @@ import { useQuery } from 'react-query';
 import useTalkApi from '@hooks/apis/useTalkApi';
 import useArticleApi from '@hooks/apis/useArticleApi';
 import useAuthApi from '@hooks/apis/useAuthApi';
-import { ArticlePostProps, ArticleProps, TalkProps } from '@utils/mockData';
 
 export const useGetAllArticles = () => {
   const { getAllArticles } = useArticleApi();
-  return useQuery<ArticleProps[], Error>(['useGetAllArticles'], () =>
-    getAllArticles(),
-  );
+  return useQuery(['useGetAllArticles'], getAllArticles);
 };
 
 export const useGetArticle = (articleId: number) => {
   const { getArticle } = useArticleApi();
-  return useQuery<ArticlePostProps, Error>(
-    ['useGetArticle'],
-    () => getArticle(articleId),
-    {
-      enabled: !!articleId,
-    },
-  );
+  return useQuery(['useGetArticle'], () => getArticle(articleId), {
+    enabled: !!articleId,
+  });
 };
 
 export const useMockFetchData = (index: number) => {
   const { mockFetchData } = useArticleApi();
-  return useQuery<any, Error>(['useMockFetchData'], () => mockFetchData(index));
+  return useQuery(['useMockFetchData'], () => mockFetchData(index));
 };
 
 export const useGetUserProfile = () => {
   const { getUserProfile } = useAuthApi();
-  return useQuery<any, Error>(['useGetUserProfile'], () => getUserProfile());
+  return useQuery(['useGetUserProfile'], getUserProfile);
 };
 
 export const useGetCheckNickname = (nickname: string) => {
   const { getCheckNickname } = useAuthApi();
-  return useQuery<any, Error>(['useGetCheckNickname'], () =>
-    getCheckNickname(nickname),
-  );
+  return useQuery(['useGetCheckNickname'], () => getCheckNickname(nickname));
 };
 
 export const useGetAllTalks = () => {
   const { getAllTalks } = useTalkApi();
-  return useQuery<TalkProps[], Error>(['useGetAllTalks'], () => getAllTalks());
+  return useQuery(['useGetAllTalks'], getAllTalks);
 };
 
 export const useGetTalk = (talkId: number) => {
   const { getTalk } = useTalkApi();
-  return useQuery<any, Error>(['useGetTalk'], () => getTalk(talkId));
+  return useQuery(['useGetTalk'], () => getTalk(talkId));
 };
