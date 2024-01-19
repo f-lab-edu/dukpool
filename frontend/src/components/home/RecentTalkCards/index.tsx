17c8f3ff326b8423ab624b2f08984a8ae9c2c714
GeonwooShin <rjsdnslawkd@naver.com>
2024-01-19 21:11:20 +0900

    refactor: useSuspense 사용으로 assert 제거

--- frontend/src/components/home/RecentTalkCards/index.tsx
+++ frontend/src/components/home/RecentTalkCards/index.tsx
@@ -6,12 +6,10 @@ import Slider from 'react-slick';
 import { MultipleRowSliderOption } from '@constants/sliderOption';
 import useMediaQuery from '@hooks/useMediaQuery';
 import { useGetAllTalks } from '@hooks/useGetQueries';
-import assert from '@utils/assert';
 
 const RecentTalkCards = memo(() => {
   const { isMobile } = useMediaQuery();
   const { data: talks } = useGetAllTalks();
-  assert(talks);
 
   const talkCards = talks.map((talk: TalkProps) => (
     <TalkCard {...talk} key={talk.id}></TalkCard>
