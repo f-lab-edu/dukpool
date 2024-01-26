c0abbf55506b8f6cc0e996811dfe48788243ddfb
GeonwooShin <rjsdnslawkd@naver.com>
2024-01-27 04:10:59 +0900

    refactor: 타입 추론되도록 수정

--- frontend/src/components/home/RecentTalkCards/index.tsx
+++ frontend/src/components/home/RecentTalkCards/index.tsx
@@ -1,6 +1,5 @@
 import { memo } from 'react';
 import styled from 'styled-components';
-import { TalkPostProps } from '@utils/mockData';
 import TalkCard from '@components/talk/TalkCard';
 import Slider from 'react-slick';
 import { MultipleRowSliderOption } from '@constants/sliderOption';
@@ -10,7 +9,7 @@ const RecentTalkCards = memo(() => {
   const { data: talks } = useGetAllTalks();
   return (
     <StyledSlider {...MultipleRowSliderOption}>
-      {talks.map((talk: TalkPostProps) => (
+      {talks.map((talk) => (
         <TalkCard {...talk} key={talk.id}></TalkCard>
       ))}
     </StyledSlider>
