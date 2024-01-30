aa779b95d02e8a385808fbc658260ae090123299
GeonwooShin <rjsdnslawkd@naver.com>
2024-01-30 18:19:29 +0900

    refactor: 카카오 로그인 컴포넌트 분리

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,10 +1,8 @@
-import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import Logo from '@assets/logo/dukpool-logo.svg';
-import KakaoLogo from '@assets/icons/kakao.svg';
-import { CONFIG } from '@config';
 import Layout from '@components/common/Layout';
 import useInnerHeight from '@hooks/useInnerHeight';
+import KakaoLoginButton from '@components/common/Button/KakaoLoginButton';
 
 const Login = () => {
   const { height } = useInnerHeight();
@@ -15,12 +13,7 @@ const Login = () => {
           <StyledLogo src={Logo} alt="Dukpool 로고" />
           <StyledInfoText>나만의 덕질 취향을 공유해보세요.</StyledInfoText>
         </StyledInfo>
-        <StyledLink
-          to={`https://kauth.kakao.com/oauth/authorize?client_id=${CONFIG.API_KEYS.LOGIN}&redirect_uri=${CONFIG.LOCAL}/api/auth/kakao/callback&response_type=code`}
-        >
-          <StyledKakaoLogo src={KakaoLogo} />
-          <StyledInfoText>카카오 로그인</StyledInfoText>
-        </StyledLink>
+        <KakaoLoginButton />
       </StyledContainer>
     </Layout>
   );
@@ -46,22 +39,7 @@ const StyledInfoText = styled.p`
   font-size: 14px;
 `;
 
-const StyledLink = styled(Link)`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 20px 50px;
-  background-color: #fee500;
-  border-radius: 12px;
-  height: 45px;
-`;
-
 const StyledLogo = styled.img`
   width: 200px;
 `;
-
-const StyledKakaoLogo = styled.img`
-  margin-right: 8px;
-`;
-
 export default Login;
