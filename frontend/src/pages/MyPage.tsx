38dc0b21fd8dc0114df702b2a89efd8bcb5fed7b
GeonwooShin <rjsdnslawkd@naver.com>
2024-01-30 18:19:51 +0900

    refactor: useToken 훅 제거

--- frontend/src/pages/MyPage.tsx
+++ frontend/src/pages/MyPage.tsx
@@ -3,11 +3,9 @@ import MyInfoSkeleton from '@components/common/Skeleton/MyInfoSkeleton';
 import MyPostsSkeleton from '@components/common/Skeleton/MyPostsSkeleton';
 import MyInfo from '@components/mypage/MyInfo';
 import MyPosts from '@components/mypage/MyPosts';
-import useToken from '@hooks/useToken';
 import { Suspense } from 'react';
 
 const MyPage = () => {
-  useToken();
   return (
     <Layout>
       <Suspense fallback={<MyInfoSkeleton />}>
