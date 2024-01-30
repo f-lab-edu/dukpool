a4ef7e69c3f95ec16e902e6bb0ab2c31cc2d6a99
GeonwooShin <rjsdnslawkd@naver.com>
2024-01-30 18:18:12 +0900

    refactor: client 분리

--- frontend/src/atoms/authAtom.ts
+++ frontend/src/atoms/authAtom.ts
@@ -1,51 +1,104 @@
-import { CONFIG } from '@config';
 import axios from 'axios';
+import { CONFIG } from '@config';
 import { atom } from 'jotai';
 import { atomWithStorage } from 'jotai/utils';
 import { jwtDecode } from 'jwt-decode';
+import {
+  ExpiredAccessTokenError,
+  ExpiredRefreshTokenError,
+  ServerError,
+} from '@utils/errors';
 
 type JwtPayload = {
-  id: number;
+  userId: string;
 };
 
 const TOKEN_KEY = 'token';
 // primitive atoms
-const tokenAtom = atomWithStorage<string | null>(TOKEN_KEY, null);
+const tokenBaseAtom = atomWithStorage<string | null>(
+  TOKEN_KEY,
+  localStorage.getItem(TOKEN_KEY) ?? null,
+);
 
 // derived atoms
 
+export const tokenAtom = atom<
+  [string | null, (newValue: string | null) => void],
+  [string | null],
+  void
+>(
+  (get, { setSelf }) => {
+    return [get(tokenBaseAtom), setSelf];
+  },
+  (get, set, value) => {
+    set(tokenBaseAtom, value);
+  },
+);
+
 // axios instance (readOnly)
-export const clientAtom = atom((get) => {
-  const token = get(tokenAtom);
+export const authClientAtom = atom((get) => {
+  const token = get(tokenBaseAtom);
   const instance = axios.create({
     baseURL: CONFIG.BASE_URL,
     headers: token ? { Authorization: `Bearer ${token}` } : {},
     withCredentials: true,
   });
+  instance.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    async (error) => {
+      if (error.code === 'EXPIRED_ACCESS_TOKEN')
+        throw new ExpiredAccessTokenError();
+      if (error.code === 'SERVER_ERROR') throw new ServerError();
+      if (error.code === 'EXPIRED_REFRESH_TOKEN')
+        throw new ExpiredRefreshTokenError();
+    },
+  );
+  return instance;
+});
+
+export const noneAuthClientAtom = atom(() => {
+  const instance = axios.create({
+    baseURL: CONFIG.BASE_URL,
+    withCredentials: true,
+  });
+  instance.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    async (error) => {
+      if (error.code === 'SERVER_ERROR') throw new ServerError();
+      if (error.code === 'EXPIRED_REFRESH_TOKEN')
+        throw new ExpiredRefreshTokenError();
+    },
+  );
   return instance;
 });
 
 // 유저 로그인 유무 (readOnly)
-export const loginStatusAtom = atom<boolean>((get) => !!get(tokenAtom));
+export const loginStatusAtom = atom<boolean>((get) => !!get(tokenBaseAtom));
 
 // 로그인 (writeOnly)
 export const loginAtom = atom(null, async (get, set) => {
-  const client = get(clientAtom);
-  const { data } = await client.get(`/auth/kakao`);
-  set(tokenAtom, data);
+  const client = get(noneAuthClientAtom);
+  const code = new URL(window.location.href).searchParams.get('code');
+  const { data } = await client.get(`/auth/kakao/callback?code=${code}`);
+  const token = data.data.accessToken;
+  set(tokenBaseAtom, token);
 });
 
 // 로그 아웃 (writeOnly)
 export const logoutAtom = atom(null, (get, set) => {
-  set(tokenAtom, null);
+  set(tokenBaseAtom, null);
 });
 
-// 유저 고유 아이디 (from JWT)
+// 유저 고유 아이디 (from JWT) (readOnly)
 export const userUniqIdAtom = atom((get) => {
-  const token = get(tokenAtom);
+  const token = get(tokenBaseAtom);
   if (token) {
     const decodeToken = jwtDecode<JwtPayload>(token);
-    return decodeToken.id;
+    return decodeToken.userId;
   }
   return null;
 });
