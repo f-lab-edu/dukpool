4061c515abb85f34ba38eb9fbcc687ff2f847a51
GeonwooShin <rjsdnslawkd@naver.com>
2024-02-02 18:45:48 +0900

    refactor: modalAtom 수정

--- frontend/src/atoms/modalAtom.ts
+++ frontend/src/atoms/modalAtom.ts
@@ -1,13 +1,3 @@
 import { atom } from 'jotai';
 
 export const modalStateAtom = atom<boolean>(false);
-
-export const modalAtom = atom<any, any, any>(
-  (get) => {
-    const modalState = get(modalStateAtom);
-    return modalState;
-  },
-  (get, set, value) => {
-    set(modalStateAtom, value);
-  },
-);
