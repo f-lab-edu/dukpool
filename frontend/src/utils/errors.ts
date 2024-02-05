e5cbe6fa1cd816e34f1e2d2ee773d785ee5c1aeb
GeonwooShin <rjsdnslawkd@naver.com>
2024-02-06 02:28:47 +0900

    feat: InvalidUrlError 생성

--- frontend/src/utils/errors.ts
+++ frontend/src/utils/errors.ts
@@ -4,3 +4,4 @@ export const UnAuthorizedError = makeError('UnAuthorizedError');
 export const ServerError = makeError('ServerError');
 export const ExpiredRefreshTokenError = makeError('ExpiredRefreshTokenError');
 export const ExpiredAccessTokenError = makeError('ExpiredAccessTokenError');
+export const InvalidUrlError = makeError('InvalidUrlError');
