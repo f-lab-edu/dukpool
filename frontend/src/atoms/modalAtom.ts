c0f01dfb0ac1f891b79df90294523ac2d51c543d
GeonwooShin <rjsdnslawkd@naver.com>
2024-02-09 20:03:43 +0900

    feat: confirmModal atom 관리

--- frontend/src/atoms/modalAtom.ts
+++ frontend/src/atoms/modalAtom.ts
@@ -1,3 +1,45 @@
 import { atom } from 'jotai';
 
 export const isModalOpenAtom = atom<boolean>(false);
+
+export const isConfirmModalAtom = atom<{
+  comment: boolean;
+  post: boolean;
+  resign: boolean;
+}>({
+  comment: false,
+  post: false,
+  resign: false,
+});
+
+export const writeConfirmModalAtom = atom(
+  null,
+  (
+    get,
+    set,
+    payload: { action: 'comment' | 'post' | 'resign'; value: boolean },
+  ) => {
+    const confirmModalState = get(isConfirmModalAtom);
+    if (payload.action === 'comment') {
+      set(isConfirmModalAtom, {
+        comment: payload.value,
+        post: confirmModalState.post,
+        resign: confirmModalState.resign,
+      });
+    }
+    if (payload.action === 'post') {
+      set(isConfirmModalAtom, {
+        comment: confirmModalState.comment,
+        post: payload.value,
+        resign: confirmModalState.resign,
+      });
+    }
+    if (payload.action === 'resign') {
+      set(isConfirmModalAtom, {
+        comment: confirmModalState.comment,
+        post: confirmModalState.post,
+        resign: payload.value,
+      });
+    }
+  },
+);
