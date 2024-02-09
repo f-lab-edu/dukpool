cafc91cc897a5f9bc4d76e2089837d2c69343712
GeonwooShin <rjsdnslawkd@naver.com>
2024-02-09 20:04:13 +0900

    refactor: 공통 레이아웃 내에 confirm 모달 추가

--- frontend/src/components/common/Layout/index.tsx
+++ frontend/src/components/common/Layout/index.tsx
@@ -2,16 +2,23 @@ import { memo } from 'react';
 import styled from 'styled-components';
 import LoginModal from '@components/common/Modal/LoginModal';
 import useModal from '@hooks/useModal';
+import useConfirmModal from '@hooks/useConfirmModal';
+import ConfirmModal from '../Modal/ConfirmModal';
 
 type LayoutProps = {
   children: React.ReactNode;
 };
 
 const Layout = memo(({ children }: LayoutProps) => {
-  const { isModal } = useModal();
+  const { isModal: isLoginModal } = useModal();
+  const { isCommentConfirm, isPostConfirm, isResignConfirm } =
+    useConfirmModal();
   return (
     <StyledLayout>
-      {isModal ? <LoginModal /> : <></>}
+      {isLoginModal ? <LoginModal /> : <></>}
+      {isCommentConfirm ? <ConfirmModal type="comment" /> : <></>}
+      {isPostConfirm ? <ConfirmModal type="post" /> : <></>}
+      {isResignConfirm ? <ConfirmModal type="resign" /> : <></>}
       {children}
     </StyledLayout>
   );
