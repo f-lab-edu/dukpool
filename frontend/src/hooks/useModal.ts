4fd9dd3e4ce17cf175f0370780ae7ffec7462371
GeonwooShin <rjsdnslawkd@naver.com>
2024-02-10 18:20:35 +0900

    refactor: Login&Confirm Modal 함께 관리

--- frontend/src/hooks/useModal.ts
+++ frontend/src/hooks/useModal.ts
@@ -1,19 +1,59 @@
-import { isModalOpenAtom } from '@atoms/modalAtom';
-import { useAtom } from 'jotai';
+import {
+  isConfirmModalAtom,
+  modalPromiseAtom,
+  writeConfirmModalAtom,
+} from '@atoms/modalAtom';
+import { useAtom, useAtomValue, useSetAtom } from 'jotai';
 
-function useModal() {
-  const [isModal, setModal] = useAtom(isModalOpenAtom);
-  // useLayoutEffect
+type ModalType = 'post' | 'comment' | 'resign' | 'login';
 
-  const openModal = () => {
-    setModal(true);
+const useModal = () => {
+  const isModal = useAtomValue(isConfirmModalAtom);
+  const setModal = useSetAtom(writeConfirmModalAtom);
+  const [modalPromise, setModalPromise] = useAtom(modalPromiseAtom);
+
+  const openModal = (type: ModalType) => {
+    return new Promise((resolve) => {
+      setModal({ action: type, value: true });
+      setModalPromise({ resolve });
+    });
   };
 
-  const closeModal = () => {
-    setModal(false);
+  const hideModal = (type: ModalType) => {
+    if (modalPromise && isModal[type]) {
+      console.log('rejected!');
+      modalPromise.resolve(false);
+      setModal({ action: type, value: false });
+      setModalPromise(null);
+    }
   };
 
-  return { isModal, openModal, closeModal };
-}
+  const confirmModal = (type: ModalType) => {
+    if (modalPromise && isModal[type]) {
+      console.log('resolved!');
+      modalPromise.resolve(true);
+      setModal({ action: type, value: false });
+      setModalPromise(null);
+    }
+  };
+
+  const hideAllModals = () => {
+    hideModal('post');
+    hideModal('comment');
+    hideModal('resign');
+    hideModal('login');
+  };
+
+  return {
+    isCommentModal: isModal.comment,
+    isPostModal: isModal.post,
+    isResignModal: isModal.resign,
+    isLoginModal: isModal.login,
+    openModal,
+    hideModal,
+    confirmModal,
+    hideAllModals,
+  };
+};
 
 export default useModal;
