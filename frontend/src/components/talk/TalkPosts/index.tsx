1925fb515dfd27b2ec549380c3e88baf750f124c
GeonwooShin <rjsdnslawkd@naver.com>
2024-02-19 21:03:13 +0900

    feat: intersectionObserver 를 사용한 무한스크롤 구현

--- frontend/src/components/talk/TalkPosts/index.tsx
+++ frontend/src/components/talk/TalkPosts/index.tsx
@@ -1,15 +1,49 @@
+import TalkCardSkeleton from '@components/common/Skeleton/TalkCardSkeleton';
 import TalkCard from '@components/talk/TalkCard';
 import { useAllTalks } from '@hooks/useGetQueries';
-import { TalkPostProps } from '@utils/mockData';
-import { memo } from 'react';
+import useIntersectionObserver from '@hooks/useIntersectionObserver';
+import { memo, useCallback, useMemo } from 'react';
 
 const TalkPosts = memo(({ sortType }: { sortType: string }) => {
-  const { data: talks } = useAllTalks(sortType);
+  const { data, hasNextPage, fetchNextPage, isFetchingNextPage } =
+    useAllTalks(sortType);
+  const talks = useMemo(
+    () => data.pages.flatMap((page) => page.data),
+    [data.pages],
+  );
+
+  const getMoreContent = async () => {
+    if (hasNextPage) await fetchNextPage();
+  };
+
+  const onIntersect = useCallback(
+    async (
+      [entry]: IntersectionObserverEntry[],
+      observer: IntersectionObserver,
+    ) => {
+      if (entry.isIntersecting && !isFetchingNextPage) {
+        observer.unobserve(entry.target);
+        await getMoreContent();
+        observer.observe(entry.target);
+      }
+    },
+    [fetchNextPage, hasNextPage],
+  );
+  const { target } = useIntersectionObserver({
+    onIntersect,
+  });
   return (
     <>
-      {talks.map((talk: TalkPostProps) => (
+      {talks.map((talk) => (
         <TalkCard key={talk.id} {...talk}></TalkCard>
       ))}
+      {isFetchingNextPage &&
+        Array.from({ length: 20 }, (_, idx) => (
+          <li key={idx}>
+            <TalkCardSkeleton />
+          </li>
+        ))}
+      <div ref={target}></div>
     </>
   );
 });
