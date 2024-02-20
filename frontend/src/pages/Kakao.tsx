a32ec5ca426644013ad31737b3627d43bba07a86
GeonwooShin <rjsdnslawkd@naver.com>
2024-02-21 00:57:16 +0900

    refactor: 로그인 이후 홈화면으로 이동

--- frontend/src/pages/Kakao.tsx
+++ frontend/src/pages/Kakao.tsx
@@ -1,12 +1,21 @@
-import { AuthContext } from '@context/AuthContext';
-import { useContext, useEffect } from 'react';
+import { loginAtom } from '@atoms/authAtom';
+import Layout from '@components/common/Layout';
+import { useSetAtom } from 'jotai';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Kakao = () => {
-  const { login } = useContext(AuthContext);
+  const login = useSetAtom(loginAtom);
+  const navigate = useNavigate();
   useEffect(() => {
     login();
+    navigate('/', { replace: true });
   }, []);
-  return <></>;
+  return (
+    <Layout>
+      <div>loading...</div>
+    </Layout>
+  );
 };
 
 export default Kakao;
