18176dee074f69bb7ec418b4410081e37da7f4a5
GeonwooShin <rjsdnslawkd@naver.com>
2024-02-21 00:35:50 +0900

    refactor: 타입 수정

--- frontend/src/@types/user.ts
+++ frontend/src/@types/user.ts
@@ -10,6 +10,6 @@ export type UserDataResponse = {
   id: string;
   profileImg: string;
   nickname: string;
-  content: Omit<Omit<ContentResponse, 'comment'>, 'writer'>;
-  talkContent: Omit<Omit<ContentResponse, 'comment'>, 'writer'>;
+  content: Omit<Omit<ContentResponse, 'comment'>, 'writer'>[];
+  talkContent: Omit<Omit<ContentResponse, 'comment'>, 'writer'>[];
 };
