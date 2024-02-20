c93b4bd7ba8449550c5bca75f8d6cde0590af876
GeonwooShin <rjsdnslawkd@naver.com>
2024-02-21 00:44:59 +0900

    refactor: 덕질자랑, 토크 상세 페이지 컴포넌트 교체

--- frontend/src/pages/TalkPost.tsx
+++ frontend/src/pages/TalkPost.tsx
@@ -1,13 +1,13 @@
 import { Suspense } from 'react';
 import Layout from '@components/common/Layout';
 import ArticlePostSkeleton from '@components/common/Skeleton/ArticlePostSkeleton';
-import DukpoolTalkPost from '@components/common/Post/DukpoolTalkPost';
+import TalkDetail from '@components/talk/TalkDetail';
 
 const TalkPost = () => {
   return (
     <Layout>
       <Suspense fallback={<ArticlePostSkeleton />}>
-        <DukpoolTalkPost />
+        <TalkDetail />
       </Suspense>
     </Layout>
   );
