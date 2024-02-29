86475b2fa37a0239a79ebd5c2ce02e28856e37ab
GeonwooShin <rjsdnslawkd@naver.com>
2024-03-01 00:35:52 +0900

    feat: mutation error 처리를 위한 useApiError 훅 생성

---
+++ frontend/src/hooks/useApiError.tsx
@@ -0,0 +1,33 @@
+import LoginModal from '@components/common/Modal/LoginModal';
+import { ModalContext } from '@context/ModalContext';
+import { ToastContext } from '@context/ToastContext';
+import {
+  ExpiredRefreshTokenError,
+  ServerError,
+  UnAuthorizedError,
+} from '@utils/errors';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const useApiError = () => {
+  const { showToast } = useContext(ToastContext);
+  const { openModal } = useContext(ModalContext);
+  const navigate = useNavigate();
+
+  const defaultMutationHandler = (error: Error) => {
+    if (error instanceof ExpiredRefreshTokenError) {
+      showToast('토큰이 만료되었습니다. 다시 로그인해주세요');
+      navigate('/login');
+    }
+    if (error instanceof ServerError) {
+      showToast('네트워크 요청에 실패했습니다. 잠시후 다시 시도해주세요.');
+    }
+    if (error instanceof UnAuthorizedError) {
+      openModal(<LoginModal />).catch(() => false);
+    }
+  };
+
+  return { defaultMutationHandler };
+};
+
+export default useApiError;
