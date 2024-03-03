ed16093606141467447842c458579cdd308ac464
GeonwooShin <rjsdnslawkd@naver.com>
2024-03-03 17:26:48 +0900

    feat: googel analytics key 추가

--- frontend/config.ts
+++ frontend/config.ts
@@ -3,6 +3,7 @@ export const CONFIG = {
   DOMAIN: process.env.DOMAIN,
   BASE_URL: process.env.BASE_URL,
   ENV: process.env.NODE_ENV,
+  GOOGLE_ANALYTICS: process.env.GOOGLE_ANALYTICS,
   API_KEYS: {
     LOGIN: process.env.KAKAO_LOGIN_API_KEY,
   },
